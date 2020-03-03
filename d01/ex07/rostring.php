0f79eb95fdb9318bd965ab8ef4d3ed0e0412f004
N0Reload <leonardo5277@hotmail.com>
2020-03-03 16:39:09 -0500

    ok

---
+++ d01/ex07/rostring.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+    if ($argc > 1)
+    {
+	    $ret = array();
+	    $ret = array_filter(explode(' ', $argv[1]));
+	    foreach(array_slice($ret, 1) as $print)
+		    echo "$print\n";
+	    foreach($ret as $print)
+	    {
+		    echo "$print\n";
+		    break;
+	    }
+    }
