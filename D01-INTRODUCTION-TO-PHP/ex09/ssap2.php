d361b225f5febd110f4ed7e6297e027eae5fb8f5
N0Reload21 <leonardo5277@hotmail.com>
2020-03-05 16:50:16 -0500

    rename folders

--- D01-INTRODUCTION-TO-PHP/ex09/ssap2.php
+++ D01-INTRODUCTION-TO-PHP/ex09/ssap2.php
@@ -1,13 +1,41 @@
 #!/usr/bin/php
 <?php
-    $ret = array();
-    unset($argv[0]);
-    foreach($argv as $str)
+
+    function ft_sort($elem1, $elem2)
     {
-    	$tmp = array_filter(explode(' ', $str));
-    	foreach($tmp as $str2)
-    		$ret[] = $str2;
+        $elem1 = strtolower($elem1);
+        $elem2 = strtolower($elem2);
+        $s = "abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        $k = 0;
+        while ($elem1[$k] && $elem2[$k])
+        {
+            $pos1 = strpos($s, $elem1[$k]);
+            $pos2 = strpos($s, $elem2[$k]);
+            if ($pos1 == $pos2)
+                ++$k;
+            else if ($pos1 > $pos2)
+                return 1;
+            else
+                return -1;
+        }
+        return 0;
     }
-    sort($ret);
-    foreach($ret as $print)
-    	echo "$print\n";
+    if ($argc > 1)
+        $array = explode(" ", $argv[1]);
+    for ($k = 2; $k < $argc; ++$k)
+    {
+        $array_new = explode(" ", $argv[$k]);
+        $size = count($array_new);
+        for ($p = 0; $p < $size; ++$p)
+            if ($array_new[$p])
+                array_push($array, $array_new[$p]);
+    }
+
+    if ($argc > 1)
+    {
+        usort($array, "ft_sort");
+        $max = count($array);
+        for ($k = 0; $k < $max; ++$k)
+            if ($array[$k])
+                printf("%s\n", $array[$k]);
+    }
