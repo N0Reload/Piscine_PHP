30ac5ec37fcd40b7d89c87a3fe5371483f3695a8
N0Reload21 <leonardo5277@hotmail.com>
2020-03-10 13:12:17 -0500

    cookie crisp

---
+++ D03-MAMP/ex03/cookie_crisp.php
@@ -0,0 +1,13 @@
+<?php
+    $action = $_GET['action'];
+    $name = $_GET['name'];
+    $value = $_GET['value'];
+
+    if (!$action || !$name)
+        return ;
+    if ($action === "get")
+        echo $_COOKIE[$name];
+    else if ($action === "del")
+        setcookie($name, null, -1);
+    else if ($action === "set")
+        setcookie($name, $value, time() + 60*60*42);
