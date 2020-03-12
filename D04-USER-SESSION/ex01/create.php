1717b0d92875fe46662015ae0a62773c97ae783e
N0Reload21 <leonardo5277@hotmail.com>
2020-03-12 18:03:42 -0500

    ok

---
+++ D04-USER-SESSION/ex01/create.php
@@ -0,0 +1,25 @@
+<?php
+    if ($_POST['login'] === "" || $_POST['passwd'] === "" && $_POST['submit'] !== "OK")
+    {
+        echo "ERROR\n";
+        return ;
+    }
+    $path = "/home/ubuntu/Desktop/Passwd";
+    if (!file_exists($path))
+        mkdir($path);
+    $path .= "/Logs";
+    if (file_exists($path))
+    {
+        $file = unserialize(file_get_contents($path));
+        foreach($file as $elem)
+            if ($elem['login'] === $_POST['login'])
+            {
+                echo "ERROR\n";
+                return ;
+            }
+    }
+    $array['login'] = $_POST['login'];
+    $array['passwd'] = hash(whirlpool, $_POST['passwd']);
+    $file[] = $array;
+    file_put_contents($path, serialize($file));
+    echo "OK\n";
