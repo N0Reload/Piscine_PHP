9846e54675d49afb911bb3930d9167ae1bded018
noreload <leonardo5277@hotmail.com>
2020-10-14 19:17:28 -0500

    storm

--- D04-USER-SESSION/ex04/chat.php
+++ D04-USER-SESSION/ex04/chat.php
@@ -3,5 +3,10 @@
     if (file_exists($path))
     {
         $db = unserialize(file_get_contents($path));
-
+        date_default_timezone_get('Europe/Paris');
+        echo '<!DOCTYPE html><html><body>';
+        foreach ($db as $value)
+        {
+            $line = "[".date(G:i)
+        }
     }
